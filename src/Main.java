53f51ddccca988a141a6124ea6e5071550a49a91
pshelly1 <peytonshelly1@gmail.com>
2021-02-26 18:40:38 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,31 @@ public class Main
         //  Ask user for temp in F. Read it in. Convert to C. Display output.
         System.out.println("Let's convert temperature!");
         System.out.print("What's the temperature in Fahrenheit? ");
-
+        float F = in.nextFloat();
+        float C = (F-32)*(5/9)
+            System.out.println("Temperature in Celsius is:");
+        
         // TODO: compute height of building
         //  Assume 20ft away. Ask how tall. Ask for angle of elevation.
         //  Compute height of building using Math.tan(). Display output.
         System.out.println("Let's roughly compute the height of a building.");
         System.out.println("Assume you are standing 20ft away.");
         System.out.print("How tall are you (ft)? ");
-
+         import java.lang.Math;
+        
+        buildingheight = Math.tan(angleElevation);
+        int feet = Integer.valueOf(height);
+        System.out.println("The height of the building is:");
         // TODO: compute probability of coveted loot.
         //  Ask for drop rate as percentage. Convert to fraction (divide by 100).
         //  Ask how many runs. Compute probability w/ formula provided.
         System.out.println("Let's compute probability of dropped loot.");
         System.out.print("What is the drop rate (%) of the loot you want? ");
+        
+        Random = new Random();
+        double probability = random.nextDouble();
+        int()=convertToFraction/100;
+        System.out.println("How many runs?");
+        
     }
 }
